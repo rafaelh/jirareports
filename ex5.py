21b5e0b52db5674266148adb7f440a8daa821749
Rafael Hart <rafael.hart9@gmail.com>
2016-12-04 10:02:32 +1100

    Exercise 6

--- ex5.py
+++ ex5.py
@@ -1,14 +1,14 @@
 my_name = 'Zed A. Shaw'
 my_age = 35 #Not a lie
-my_height = 74 #Inches
-my_weight = 180 #lbs
+my_height = 74 * 0.0833 #Inches to feet
+my_weight = 180 * 0.454 #lbs to kgs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
 print "Let's talk about %s" % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "He's %d feet tall." % my_height
+print "He's %d kgs heavy." % my_weight
 print "Actually that's not too heavy"
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
