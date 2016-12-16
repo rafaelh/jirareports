613037b0291db3425bffa4d6d0b7a1213db58b3d
Rafael Hart <rafael.hart9@gmail.com>
2016-12-16 13:05:17 +1100

    Experimenting

---
+++ gitupdate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+
+# Set home directory path
+gitrepos = os.listdir("/home/rafael/")
+
+print gitrepos
+
+#List items in the gitrepo directory
+#Loop through items
+    #Update git repo command
+#report done
+
